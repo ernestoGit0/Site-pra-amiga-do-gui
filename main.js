a72e38bc718bc9fbd5e921e307bcef59928326a3
Ernesto Raab <ernestoraabszirmai@gmail.com>
2022-03-15 15:13:27 -0300

    commit

---
+++ main.js
@@ -0,0 +1,7 @@
+ let carta = document.querySelector("#carta");
+
+
+carta.addEventListener('click', alert(msg));
+
+
+let msg = "Bem esses 3 anos e 1 ano de namoro passaram rapido, e eu agradeço a deus por ter te conhecido e por ter tido vc como primeira namorada, serio todas as noites eu agradecia e pedia o bem para vc, raramente esquecia de pedir mas ate hoje n deixo de te inlcuir nas orações e sempre to pedindo o seu bem, as vezes ate me excluindo delas e focando em vc Lembre se de que a gente ainda te ama e estamos tentando fazer de tudo pra vc melhorar!";
